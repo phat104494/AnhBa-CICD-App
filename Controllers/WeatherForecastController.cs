62a81ed1805489815823cd937e6c5edb8fde3632
phatdang1404 <phat104494@donga.edu.vn>
2026-09-25 21:43:14 +0700

    Test automatic CI CD deployment

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace AnhBa_CICD_App.Controllers
     {
         private static readonly string[] Summaries =
         [
-            "Very Cold", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "CI-CD SUCCESS", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         ];
 
         [HttpGet(Name = "GetWeatherForecast")]
